0277d05654fe7b6ba1e5d41dbd7cf0f7d2428a75
Lukas Nagel <lukas@lna-dev.net>
2024-02-15 19:16:33 +0100

    🐛 Updated activity model to camelCase

--- src/activityPub/model.go
+++ src/activityPub/model.go
@@ -3,17 +3,17 @@ package activityPub
 import "time"
 
 type Activity struct {
-	Id        string
-	Type      string
-	Context   interface{}
-	To        []string
-	Bto       []string
-	Cc        []string
-	Bcc       []string
-	Audience  []string
-	Object    ActivityObject
-	Published time.Time
-	Actor     string
+	Id        string         `json:"id"`
+	Type      string         `json:"type"`
+	Context   interface{}    `json:"context"`
+	To        []string       `json:"to"`
+	Bto       []string       `json:"bto"`
+	Cc        []string       `json:"cc"`
+	Bcc       []string       `json:"bcc"`
+	Audience  []string       `json:"audience"`
+	Object    ActivityObject `json:"object"`
+	Published time.Time      `json:"published"`
+	Actor     string         `json:"actor"`
 }
 
 type ActivityObject interface {
